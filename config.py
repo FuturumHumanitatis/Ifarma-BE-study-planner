ec19a8d78c8dd8ad8f4ce34779dd9b0cda278057
DavidAzaryan <124185952+FuturumHumanitatis@users.noreply.github.com>
2026-02-25 15:04:21 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+DEFAULT_ALPHA = 0.05
+DEFAULT_POWER = 0.80
+THETA_LOWER = 0.80
+THETA_UPPER = 1.25
+
+DESIGN_PENALTY_MULTIPLIER = {
+    "2x2": 1.0,
+    "2x3x3": 1.1,  # 3-way replicate
+    "2x4": 1.2,    # 4-way replicate
+    "parallel": 1.3
+}
